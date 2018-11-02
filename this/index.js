920e290c55f07a143de52c5381f43bfaf2d06730
prescottbreeden <prescott.breeden@gmail.com>
2018-11-02 14:39:15 -0700

    examples explaining how this works in javascript

---
+++ this/index.js
@@ -0,0 +1,61 @@
+const video = {
+  title: 'a',
+  play() {
+    console.log(this);
+  }
+}
+
+video.stop = function() {
+  console.log(this);
+}
+
+video.stop()
+
+console.log('\n\n===================================\n\n')
+
+function playVideo() {
+  console.log(this);
+}
+
+playVideo();
+
+
+function Video(title) {
+  this.title = title;
+  console.log(this);
+}
+console.log('\n\n===================================\n\n')
+const v = new Video('a');
+
+
+console.log('\n\n===================================\n\n')
+
+const video2 = {
+  title: 'a',
+  tags: ['a', 'b', 'c'],
+  showTags() {
+    this.tags.forEach(function(tag) {
+      console.log(tag);
+      // console.log(this);
+    });
+  },
+  showTags2() {
+    this.tags.forEach(tag => {
+      console.log(this);
+    })
+  }
+};
+
+video2.showTags();
+video2.showTags2();
+
+console.log('\n\n===================================\n\n')
+
+const cat = {
+  name: 'Mittens',
+  meow: function() {
+    console.log(this)
+  }
+}
+
+cat.meow();
