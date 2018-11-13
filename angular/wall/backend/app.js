92f075e76e421240bdaf67f5a23f411dbefae8c3
prescottbreeden <prescott.breeden@gmail.com>
2018-11-12 22:17:43 -0800

    server app edits for api post request

--- angular/wall/backend/app.js
+++ angular/wall/backend/app.js
@@ -18,15 +18,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully',
-    posts: post
-  });
-});
-
 app.get('/api/posts', (req, res, next) => {
   const posts = [
     { id: 'fad134145i',
@@ -37,13 +28,22 @@ app.get('/api/posts', (req, res, next) => {
       title: 'Second server-side post',
       content: 'this is coming from the server...' 
     }
-
   ];
   res.status(200).json({
     message: 'Posts fetched successfully!',
     posts: posts
   });
 });
+
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully',
+    posts: post
+  });
+});
+
 
 
 module.exports = app;
